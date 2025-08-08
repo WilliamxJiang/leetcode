a04b53db3d0a2ba47c9025e2bf993fa240d7f128
William Jiang <75504612+WilliamxJiang@users.noreply.github.com>
2025-08-08 17:15:10 -0400

    Create 2809-CreateHelloWorldFunction

---
+++ 2809-CreateHelloWorldFunction/2809-CreateHelloWorldFunction.js
@@ -0,0 +1,14 @@
+// Last updated: 8/8/2025, 5:15:09 PM
+/**
+ * @return {Function}
+ */
+var createHelloWorld = function() {
+    return function(...args) {
+        return "Hello World"
+    }
+};
+
+/**
+ * const f = createHelloWorld();
+ * f(); // "Hello World"
+ */
