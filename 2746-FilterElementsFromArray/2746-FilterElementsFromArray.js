58cd5e674979c9baa19562934567cbb0a8a47a54
William Jiang <75504612+WilliamxJiang@users.noreply.github.com>
2025-08-08 17:15:13 -0400

    Create 2746-FilterElementsFromArray

---
+++ 2746-FilterElementsFromArray/2746-FilterElementsFromArray.js
@@ -0,0 +1,15 @@
+// Last updated: 8/8/2025, 5:15:13 PM
+/**
+ * @param {number[]} arr
+ * @param {Function} fn
+ * @return {number[]}
+ */
+var filter = function(arr, fn) {
+    let filteredArr = []
+    for (let i = 0; i < arr.length; i++) {
+        if (fn(arr[i], i)) {
+            filteredArr.push(arr[i]);
+        }
+    }
+    return filteredArr;
+};
