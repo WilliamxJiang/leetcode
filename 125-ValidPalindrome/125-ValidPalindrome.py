f651c38b18d8c11e6fdbe1c6c3ed5c062527a075
William Jiang <75504612+WilliamxJiang@users.noreply.github.com>
2025-08-08 17:15:15 -0400

    Create 125-ValidPalindrome

---
+++ 125-ValidPalindrome/125-ValidPalindrome.py
@@ -0,0 +1,12 @@
+# Last updated: 8/8/2025, 5:15:14 PM
+class Solution:
+    def isPalindrome(self, s: str) -> bool:
+        cleaned = ''
+
+        for char in s:
+            if char.isalnum():
+                cleaned += char.lower()
+        
+        if cleaned == cleaned[::-1]:
+            return True
+        return False
