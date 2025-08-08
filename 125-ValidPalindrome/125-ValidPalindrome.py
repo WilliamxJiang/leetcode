d0adfcce3dc65f9901362c256cd7b9d8c04dff55
William Jiang <75504612+WilliamxJiang@users.noreply.github.com>
2025-08-08 17:49:59 -0400

    File updated at 8/8/2025, 5:49:59 PM

--- 125-ValidPalindrome/125-ValidPalindrome.py
+++ 125-ValidPalindrome/125-ValidPalindrome.py
@@ -1,12 +1,13 @@
-# Last updated: 8/8/2025, 5:15:14 PM
+# Last updated: 8/8/2025, 5:49:59 PM
 class Solution:
-    def isPalindrome(self, s: str) -> bool:
-        cleaned = ''
+    def maxProfit(self, prices: List[int]) -> int:
+        min_price = prices[0]
+        max_profit = 0
 
-        for char in s:
-            if char.isalnum():
-                cleaned += char.lower()
-        
-        if cleaned == cleaned[::-1]:
-            return True
-        return False
+        for price in prices:
+            if min_price > price:
+                min_price = price
+            profit = price - min_price
+            if max_profit < profit:
+                max_profit = profit
+        return max_profit
