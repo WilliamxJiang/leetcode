23e5633dd679a8ef9a7c647f11e6eedad26b7986
William Jiang <75504612+WilliamxJiang@users.noreply.github.com>
2025-08-09 16:36:35 -0400

    File updated at 8/9/2025, 4:36:35 PM

--- 424-LongestRepeatingCharacterReplacement/424-LongestRepeatingCharacterReplacement.py
+++ 424-LongestRepeatingCharacterReplacement/424-LongestRepeatingCharacterReplacement.py
@@ -1,19 +1,18 @@
-# Last updated: 8/9/2025, 4:10:21 PM
+# Last updated: 8/9/2025, 4:36:35 PM
 class Solution:
     def characterReplacement(self, s: str, k: int) -> int:
         count = defaultdict(int)
 
-        l = 0
-        maxCount = 0
-        result = 0
+        left = 0
+        max_count= 0
+        best = 0
 
         for r, char in enumerate(s):
             count[char] += 1
-            maxCount = max(maxCount, count[char])
-
-            #if replacements needed > k
-            while (r-l+1) - maxCount > k:
-                count[s[l]] -= 1
-                l += 1
-            best = max(maxCount, r-l+1)
+            max_count = max(max_count, count[char])
+        
+            while (r-left + 1) - max_count > k:
+                count[s[left]] -= 1
+                left += 1
+            best = max(best, r-left + 1)
         return best
