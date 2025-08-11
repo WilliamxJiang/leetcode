fb28a93a3f0493dd87e26cefb89b94aff4862aad
William Jiang <75504612+WilliamxJiang@users.noreply.github.com>
2025-08-11 10:07:31 -0400

    File updated at 8/11/2025, 10:07:31 AM

--- 5-LongestPalindromicSubstring/5-LongestPalindromicSubstring.py
+++ 5-LongestPalindromicSubstring/5-LongestPalindromicSubstring.py
@@ -1,22 +1,24 @@
-# Last updated: 8/11/2025, 9:49:18 AM
+# Last updated: 8/11/2025, 10:07:31 AM
+
 class Solution:
-    def longestPalindrome(self, s: str) -> str:
-        n = len(s)
-        best_l = 0
-        best_r = 0
-        for center in range(n):
-            #iterate over every possible center
-            l = r = center
-            while  l >= 0 and r < n and s[l] == s[r]:
-                if (r-l) > (best_r - best_l):
-                    best_l = l
-                    best_r = r
-                l -=1 
-                r += 1
-            l, r = center, center + 1
-            while l >= 0 and r < n and s[l] == s[r]:
-                if (r-l) > (best_r-best_l):
-                    best_l, best_r = l, r
-                l -= 1
-                r += 1
-        return s[best_l:best_r+1]
+    def merge(self, intervals: List[List[int]]) -> List[List[int]]:
+        #edge cases return single interval
+
+        if not intervals:
+            return[]
+        
+        #sort by start time
+        intervals = sorted(intervals, key=lambda x:x[0])
+        merged: List[List[int]] = []
+        
+        #seed with the first interval so we can look back at merged[-1]
+        merged.append(intervals[0])
+        for i in range(1, len(intervals)):
+            start, end = intervals[i] 
+            last_start, last_end = merged[-1]
+            if start <= last_end:
+                merged[-1][1] = max(last_end, end) #extend the end, keep the start the same cuz we sorted the start
+            else:
+                merged.append([start, end])
+
+        return merged
